4b3a735946a057bdb62c17a0d0d8d4c534f60b7d
Matthew <matthewgoodall1579@gmail.com>
2018-04-17 10:01:16 -0700

    Semi fixed blocks

--- src/entities/DirtBlock.java
+++ src/entities/DirtBlock.java
@@ -9,8 +9,8 @@ import models.TexturedModel;
 
 public class DirtBlock extends Block {
 
-	static Texture dirtTexture = new Texture(Loader.loadTexture("grass"));
-	static RawModel cubeModel = Loader.loadOBJ("cube");
+	static Texture dirtTexture = new Texture(Loader.loadTexture("dirtBlockTexture"));
+	static RawModel cubeModel = Loader.loadOBJ("block");
 	static TexturedModel dirtModel = new TexturedModel(cubeModel, dirtTexture);
 	
 	public DirtBlock(Vector3f position) {
